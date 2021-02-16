694ff71ed63227f914d1c2c000a3098e67dd0134
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2021-02-16 15:21:09 +0100

    latest commit

---
+++ build/precache-manifest.2d055435649e72edf724833193c6b8c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22619511a93646522b362e2b6613e5b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90df4ebacaf0499e8814",
+    "url": "/static/css/2.b080d97b.chunk.css"
+  },
+  {
+    "revision": "01e1e3f576755c54bc6e",
+    "url": "/static/css/main.450593e4.chunk.css"
+  },
+  {
+    "revision": "90df4ebacaf0499e8814",
+    "url": "/static/js/2.d92d227f.chunk.js"
+  },
+  {
+    "revision": "aa0609ef86f8668f5453c3d25766aa22",
+    "url": "/static/js/2.d92d227f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e1e3f576755c54bc6e",
+    "url": "/static/js/main.139ddb4e.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
